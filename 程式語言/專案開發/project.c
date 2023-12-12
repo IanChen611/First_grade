52001c2e75cdeba4f46d3deda6ad1e30c10af4bc
Ottoelif <ianchen611@gmail.com>
2023-12-12 23:35:09 +0800

    version1.1

--- 程式語言/專案開發/project.c
+++ 程式語言/專案開發/project.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<stdbool.h>
 #include<string.h>
+#include<stdlib.h>
 
 typedef struct node_s{
     int num; //節點編號
@@ -46,7 +47,7 @@ int get_node_time(node *n){
     if(n->path == NULL){
         return n->time;
     }
-    return get_last_path(n->path, n);//找該path上至node n的時間
+    return cla_path_time(n->path, n);//找該path上至node n的時間
 }
 
 path* newpath(path *root, node *now, node *from_node){
@@ -75,18 +76,84 @@ path* newpath(path *root, node *now, node *from_node){
 
 
 int main(){
-    node *list, now_ptr, now_child_ptr;//nodearray, 目前要處理的node, 該子節點
-    path root_path, nowpath_ptr;
-    int n;//有幾個專案=>有幾個node
-    scanf("%d", &n);
-    //將list初始化為0
-    list = malloc(n * sizeof(node));
-    memset(list, 0, n*sizeof(node));
+    node *list, *now, *now_1;//nodearray, 目前要處理的node, 該子節點
+    path root_path, *p, *tmp_path;
+    int N;//有幾個專案=>有幾個node
+    char buffer[256], *buffer_pointer;
+    scanf("%d", &N);
+    //將list和root_path初始化為0
+    memset(&root_path, 0, sizeof(root_path));
+    list = malloc(N * sizeof(node));
+    memset(list, 0, N*sizeof(node));
     //list 中 0 跳過
     //所以num為編號1~n
-    for(int i=1;i<=n;i++){
+    for(int i=0;i<N;i++){
         int time, many;
         scanf("%d %d", &time, &many);
-        now_ptr = list[i];
+        printf("time = %d, many = %d\n", time, many);
+        now = &list[i];
+        now->num = i;
+        now->time = time;
+        //now後面沒接東西
+        if(now->next==NULL){
+            newpath(&root_path, now, NULL);
+        }
+        for(int j=0;j<many;j++){
+            int tem;
+            scanf("%d", &tem);
+            printf("tem = %d\n", tem);
+            now_1 = &list[tem-1];
+            if(now->next == NULL){
+                //當now->next為空時，但下個又要接東西
+                //將now_1歸屬於與now的path同一條
+                //再將now的next改成now1
+                now->next = now_1;
+                now_1->path = now->path;
+            }
+            else{
+                /*父節點有兩條或兩條以上的岔路時
+                路徑新增將now_1為新的起點
+                該路的時間是根據父節點的時間
+                所以參數的父節點放的是now
+                */
+                newpath(&root_path, now_1, now);
+            }
+        }
     }
+
+    path *now_2 = &root_path;
+    int tt = 0;
+    while(now_2!=NULL){
+        printf("now_2 num = %d\n", now_2->first->num);
+        printf("father_path_time = %d\n", now_2->from_time);
+        now_2 = now_2->next_path;
+    }
+
+    int longest = 0;
+    //將p(class為path*)設為root(path的起點)的地址
+    p = &root_path;
+    int t;//等等放時間用的
+    while(p!=NULL){
+        t = cla_path_time(p, NULL);
+        if(t>longest) longest = t;
+        p = p->next_path;
+    }
+    printf("%d\n", longest);
+
+    p = root_path.next_path;
+    while(p!=NULL){
+        tmp_path = p;
+        p = p->next_path;
+        free(tmp_path);
+    }
+    free(list);
 }
+
+/*
+5
+6 2 2 3
+5 1 4
+11 1 5
+4 1 5
+8 0
+*/
